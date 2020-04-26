9b0ea0119a530ebd0a2a939a2092a0abdbd616af
Sergiy Morenets <sergey.morenets@gmail.com>
2020-04-26 16:33:07 +0300

    Task #17

--- hibernate-task15/src/main/java/org/it/discovery/training/hibernate/naming/UpperCaseNamingStrategy.java
+++ hibernate-task15/src/main/java/org/it/discovery/training/hibernate/naming/UpperCaseNamingStrategy.java
@@ -3,7 +3,9 @@ package org.it.discovery.training.hibernate.naming;
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UpperCaseNamingStrategy extends PhysicalNamingStrategyStandardImpl {
     @Override
     public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
