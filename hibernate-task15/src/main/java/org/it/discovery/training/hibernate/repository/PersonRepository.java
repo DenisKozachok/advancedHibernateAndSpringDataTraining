b48c37a259df906dc8bb273181ea5615548d1a99
Sergiy Morenets <sergey.morenets@gmail.com>
2020-04-26 17:19:23 +0300

    Task #18

--- hibernate-task15/src/main/java/org/it/discovery/training/hibernate/repository/PersonRepository.java
+++ hibernate-task15/src/main/java/org/it/discovery/training/hibernate/repository/PersonRepository.java
@@ -1,47 +1,51 @@
 package org.it.discovery.training.hibernate.repository;
 
 import org.it.discovery.training.hibernate.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PersonRepository {
+public interface PersonRepository extends JpaRepository<Person, Integer> {
 
 	/**
 	 * Returns all the persons that haven't written books
 	 *
 	 * @return
 	 */
+	@Query("FROM Person p WHERE p.books is empty")
 	List<Person> findPersonWithoutBooks();
 
 	/**
 	 * Returns author who wrote maximum number of books
 	 * @return
 	 */
-	Person findMostEfficientAuthor();
-	
+	//Person findMostEfficientAuthor();
+
 	/**
 	 * Returns all the persons which books contain the specified text in
 	 * their name
+	 *
 	 * @param name
 	 * @return
 	 */
-	List<Person> findPersonsByBook(String name);
-	
+	List<Person> findPersonsByBooks_name(String name);
+
 	/**
 	 * Returns all the persons that have written books prepared by
 	 * this publisher
 	 * @param number
 	 * @return
 	 */
-	List<Person> findPersonsOfPublisher(int publisherId);
-	
+	//List<Person> findPersonsOfPublisher(int publisherId);
+
 	/**
 	 * Find persons(limit or less) with maximum number of books 
 	 * ordered by number of books)
 	 * @param limit
 	 * @return
 	 */
-	List<Person> findTopAuthors(int limit);
-	
+	//List<Person> findTopAuthors(int limit);
+
 
 }
